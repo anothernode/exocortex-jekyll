592bcb7a5d12a8550f6c3fe2164c22cde32b777b
Moritz Reiter <moritz@anothernode.com>
2019-02-28 13:34:45 +0100

    Add git hook for deployment

---
+++ pre-push-git-hook.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Move me to .git/hooks/pre-push
+
+set -e
+
+project_dir=$(git rev-parse --show-toplevel)
+site_dir=${project_dir}"/_site"
+remote_dir="exocortex.anothernode.com:~/exocortex"
+
+cd ${project_dir}
+bundle exec jekyll build
+
+echo "-----------------------------------------"
+echo " 🛠  Site successfully built with Jekyll "
+echo "-----------------------------------------"
+
+scp -r ${site_dir}/* ${remote_dir}
+
+echo "-------------------------------------"
+echo " 🏗  Exocortex successfully deployed "
+echo "-------------------------------------"
+
